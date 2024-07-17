f30214c85fb7c5686584196d9637046da87b0f9b
kotadiasmit <smit.k@brilworks.com>
2024-07-17 12:06:26 +0530

    agent id changed 151

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,11 +58,11 @@ export default function RootLayout({ children }) {
           <Script
             defer
             id="chatbot"
-          >{`window.chatBotConfig = {agentId: 274}`}</Script>
+          >{`window.chatBotConfig = {agentId: 151}`}</Script>
           <Script
             defer
             id="chatbot-widget-script"
-            src="https://dev.swiftsupport.ai/ChatbotScripts/chatbotBubble.js"
+            src="https://app.swiftsupport.ai/ChatbotScripts/chatbotBubble.js"
           />
         </head>
         <body suppressHydrationWarning={true}>
