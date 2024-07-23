ebd7be622a00df860d98249c34ea1735e2d85dcf
kotadiasmit <smit.k@brilworks.com>
2024-07-23 18:58:14 +0530

    swiftsupport chatbot script added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,20 +3,20 @@ import "./globals.css";
 import "./styles/Animation.scss";
 import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
 import StoryblokProvider from "./components/StoryblokProvider";
-import { Outfit } from 'next/font/google'
+import { Outfit } from "next/font/google";
 import Header from "./components/Header";
 import LoadScripts from "./ScriptLoader";
+import Script from "next/script";
 
 storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
   use: [apiPlugin],
 });
 const outfit = Outfit({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--global-font',
-})
-
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--global-font",
+});
 
 export const metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL),
@@ -29,19 +29,29 @@ export default function RootLayout({ children }) {
     <StoryblokProvider>
       <html lang="en" className={`${outfit.variable}`}>
         <head>
-
           {process.env.VERCEL_ENV === "production" && (
-            <meta
-              name="google-site-verification"
-              content="c0Wa7YSps4FhBSg1lMMWnE7_livU8FgBZAbCRtwW6JE"
-            />
+            <>
+              <meta
+                name="google-site-verification"
+                content="c0Wa7YSps4FhBSg1lMMWnE7_livU8FgBZAbCRtwW6JE"
+              />
+              <Script
+                defer
+                id="chatbot"
+              >{`window.chatBotConfig = {agentId: 317}`}</Script>
+              <Script
+                defer
+                id="chatbot-widget-script"
+                src="https://dev.swiftsupport.ai/ChatbotScripts/chatbotBubble.js"
+              />
+            </>
           )}
         </head>
         <body suppressHydrationWarning={true}>
           <Header />
           {children}
           <Footer />
-          <LoadScripts  />
+          <LoadScripts />
         </body>
       </html>
     </StoryblokProvider>
