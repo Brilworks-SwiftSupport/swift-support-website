9fc7e7c18e1ae2d4491d51e57305b02cf1b41d4c
shivsagar <shiv.s@brilworks.com>
2024-07-23 16:20:50 +0530

    ui issue fixed

--- src/app/(pages)/blog/page.js
+++ src/app/(pages)/blog/page.js
@@ -37,7 +37,7 @@ export async function generateMetadata() {
 
 const page =({ searchParams })  => {
   return (
-    <div className="bg_contactus min-h-[90vh]">
+    <div className="bg_contactus min-h-[100vh]">
     <BlogListPage searchParams={searchParams} />;
     </div>
   );
