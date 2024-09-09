a190f39545fc0c777b0c61bd1754432c1f9ef526
shivsagar <shiv.s@brilworks.com>
2024-09-09 13:50:48 +0530

    updated

--- src/app/ScriptLoader.js
+++ src/app/ScriptLoader.js
@@ -1,28 +1,30 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import Script from "next/script";
-import { useMediaQuery } from "react-responsive";
 
-const LoadScripts = () => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const [scriptsLoaded, setScriptsLoaded] = useState(false);
+const LoadScripts = ({}) => {
+  const [script, setScript] = useState(<></>);
 
   useEffect(() => {
-    if (!isMobile) {
-      const triggerChatbot = () => {
-        const chatbotButton = document.getElementById("swiftSupport-bubble-button");
-        chatbotButton?.click();
-      };
+    const triggerChatbot = () => {
+      const chatbotButton = document.getElementById(
+        "swiftSupport-bubble-button"
+      );
+      if (chatbotButton) {
+        chatbotButton.click();
+      }
+    };
 
-      const timeoutId = setTimeout(triggerChatbot, 500);
-      return () => clearTimeout(timeoutId);
-    }
-  }, [isMobile]);
+    const timeoutId = setTimeout(triggerChatbot, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   useEffect(() => {
     const loadScripts = () => {
-      setScriptsLoaded(true);
+      setTimeout(() => {
+        setScript(<ScriptAnalytics />);
+      }, 3000); // Adjust the delay as needed (3000ms = 3 seconds)
     };
 
     if (document.readyState === "complete") {
@@ -33,24 +35,24 @@ const LoadScripts = () => {
     }
   }, []);
 
-  if (!scriptsLoaded) return null;
-
-  return <ScriptAnalytics />;
+  return script;
 };
 
 export default LoadScripts;
 
-const ScriptAnalytics = () => (
-  <>
-    <Script defer id="tag-manager">
-      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+const ScriptAnalytics = () => {
+  return (
+    <>
+      <Script defer id="tag-manager">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}')`}
-    </Script>
-    <Script defer id="fb-pixel">
-      {`!function(f,b,e,v,n,t,s)
+      </Script>
+
+      <Script defer id="fb-pixel">
+        {`!function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -60,13 +62,15 @@ const ScriptAnalytics = () => (
       'https://connect.facebook.net/en_US/fbevents.js');
       fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
       fbq('track', 'PageView')`}
-    </Script>
-    <Script defer id="clarity">
-      {`(function(c,l,a,r,i,t,y){
+      </Script>
+
+      <Script defer id="clarity">
+        {`(function(c,l,a,r,i,t,y){
       c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
       t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
       y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
       })(window, document, "clarity", "script", '${process.env.NEXT_PUBLIC_CLARITY_ID}');`}
-    </Script>
-  </>
-);
+      </Script>
+    </>
+  );
+};
