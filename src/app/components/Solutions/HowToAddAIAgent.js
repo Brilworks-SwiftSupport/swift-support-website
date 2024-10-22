2b734f151b4a2564226f816f2eaa8906de500173
Manish5900 <manish.s@brilworks.com>
2024-10-22 18:37:01 +0530

    Updated solution page and updated sitemap

--- src/app/components/Solutions/HowToAddAIAgent.js
+++ src/app/components/Solutions/HowToAddAIAgent.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -7,27 +6,40 @@ const HowToAddAIAgent = ({ howToAddAIAgentData }) => {
     howToAddAIAgentData?.[0];
 
   return (
-    <div className="solutions xl:py-[90px] lg:py-[60px] md:py-10 py-8 !px-4 lg:px-0">
-      <div className="container max-w-[1080px] mx-auto">
-        <h2 className="mb-5 md:text-center text-left">{title}</h2>
-        <p className="md:mb-10 mb-8 text-center">{description}</p>
-        <div className="flex items-center justify-center flex-col">
-          <div className="lg:mb-[70px] md:mb-[50px] mb-9">
+    <div
+      className="solutions xl:py-[90px] lg:py-[60px] md:py-10 py-8 !px-4 lg:px-0 relative"
+      style={{
+        backgroundImage: `url(${banner_image?.filename})`,
+        backgroundPosition: "center",
+        backgroundSize: "fill",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-80 z-0"></div>
+      <div className="relative container max-w-[1080px] mx-auto z-10">
+        <h2 className="mb-5 md:text-center text-left text-colorWhite">
+          {title}
+        </h2>
+        <p className="md:mb-10 mb-8 text-center text-colorWhite">
+          {description}
+        </p>
+        <div className="flex items-center justify-center flex-col z-20">
+          <div>
             <Link
               href={youtube_link?.url}
               target="_blank"
-              className="flex items-center justify-center w-fit font-semibold border-colorBlack border bg-colorBlack text-colorWhite rounded-[30px] px-6 py-4 text-xl"
+              className="flex items-center justify-center w-fit font-semibold border-colorWhite border bg-colorWhite text-colorBlack rounded-[30px] px-6 py-4 text-xl cursor-pointer"
             >
               {button_text}
             </Link>
           </div>
-          <Image
+          {/* <Image
             className="h-full"
             src={banner_image?.filename}
             alt={banner_image?.alt || "how-to-add-ai-agent-image"}
             width="1080"
             height="506"
-          />
+          /> */}
         </div>
       </div>
     </div>
