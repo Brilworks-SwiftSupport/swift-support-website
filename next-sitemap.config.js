359f68a9a907cbede68d55cfad36e05e01f814f1
hasmukh2023 <hasmukh.m@brilworks.com>
2024-12-30 12:01:45 +0530

    youtube tools sitemap added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -12,7 +12,30 @@ const slugify = (text) => {
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://www.swiftsupport.ai",
-    generateRobotsTxt: true, // Generate robots.txt file
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                disallow: '/?s=',
+            },
+            {
+                userAgent: '*',
+                disallow: '/search/',
+            },
+            {
+                userAgent: '*',
+                disallow: '/next/static',
+            },
+        ],
+        additionalSitemaps: [
+            `${process.env.NEXT_PUBLIC_BASE_URL || "https://www.swiftsupport.ai/"}sitemap.xml`, // Add sitemap with dynamic URL
+        ],
+    },
     sitemapSize: 5000, // Split large sitemaps
     generateIndexSitemap: false, // If you don't want an index sitemap
     exclude: [], // Paths to exclude from the sitemap
